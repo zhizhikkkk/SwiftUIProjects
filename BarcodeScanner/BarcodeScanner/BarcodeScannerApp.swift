20d8ed8234a736ec893bcddad5f241856482afa6
Zhanel <98708550+zhizhikkkk@users.noreply.github.com>
2024-11-14 16:13:05 +0500

    feat : created new project BarcodeScanner

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Zhanel Mamyt on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
