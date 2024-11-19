2dcba02d53a2126421471d67432e41761b7bf7ba
Zhanel <98708550+zhizhikkkk@users.noreply.github.com>
2024-11-19 14:43:55 +0500

    feat : finished project barcode scanner

---
+++ BarcodeScanner/BarcodeScanner/BarcodeScannerViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  BarcodeScannerViewModel.swift
+//  BarcodeScanner
+//
+//  Created by Zhanel Mamyt on 19.11.2024.
+//
+
+import SwiftUI
+
+final class BarcodeScannerViewModel: ObservableObject {
+    
+    @Published var scannedCode = ""
+    @Published var alertItem: AlertItem?
+    
+    var statusText: String {
+        scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode
+    }
+    
+    var statusTextColor: Color {
+        scannedCode.isEmpty ? .red : .green
+    }
+}
+
